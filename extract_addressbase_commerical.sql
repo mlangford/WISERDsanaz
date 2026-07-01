7ebf5e1589b5ee7479c966925e55ce43e0ed9598
mitch langford <mitchel.langford@southwales.ac.uk>
2026-07-01 10:54:28 +0100

    Create ab_commercial table from ab_wales data
    extract the address points that are designated as commercial
    we think this is dated about 2022, 
    and is using the addressbase premium product

---
+++ extract_addressbase_commerical.sql
@@ -0,0 +1,19 @@
+drop table if exists addbase.ab_commercial;
+create table addbase.ab_commercial as 
+SELECT id,
+       field_1 AS "id2",
+       field_5 AS "uprn",
+       classification,
+       xcoord,
+       ycoord,
+       latitude,
+       longitude,
+       address,
+       postcode,
+       St_setsrid(St_makepoint(xcoord::real, ycoord::real), 27700) as "geom"
+FROM   addbase.ab_wales
+WHERE  classification LIKE 'C%';
+
+create index on addbase.ab_commercial using gist(geom);
+create index on addbase.ab_commercial (id);
+create index on addbase.ab_commercial (classification);
